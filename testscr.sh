741e6de0108c6a5f749378998de7afb176ccf8e1
root <root@ip-172-31-91-69.ec2.internal>
2019-12-29 05:19:24 +0000

    testcommit 15

--- testscr.sh
+++ testscr.sh
@@ -3,5 +3,6 @@ echo "hello world"
 varImage_name=`cat test |grep -i tag|awk -F : '{print $2}'`
 varIp_address=`cat test |grep -i ip-address|awk -F : '{print $2}'`
 echo $varImage_name
+echo $varIp_address
 scp -pr test ec2-user@54.161.192.128:~/
 ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
