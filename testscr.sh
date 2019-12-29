d2ced92345a144bad14824beec5824c23495f2aa
root <root@ip-172-31-91-69.ec2.internal>
2019-12-29 05:15:32 +0000

    testcommit 14

--- testscr.sh
+++ testscr.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "hello world"
-cd /var/lib/jenkins/workspace/github
-varImage_name=`cat /var/lib/jenkins/workspace/github/test |grep -i tag|awk -F : '{print $2}'`
-varIp_address=`cat /var/lib/jenkins/workspace/github/test |grep -i ip-address|awk -F : '{print $2}'`
+varImage_name=`cat test |grep -i tag|awk -F : '{print $2}'`
+varIp_address=`cat test |grep -i ip-address|awk -F : '{print $2}'`
 echo $varImage_name
-scp -pr /var/lib/jenkins/workspace/github/test ec2-user@54.161.192.128:~/
-ssh ec2-user@$varIp_address "docker run `cat /var/lib/jenkins/workspace/github/test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
+scp -pr test ec2-user@54.161.192.128:~/
+ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
