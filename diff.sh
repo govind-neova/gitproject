40c58e062fe6be48bd3a9c19bae8ef3b6c1da1ee
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 19:01:52 +0530

    delete content from file testscr.sh

--- diff.sh
+++ diff.sh
@@ -6,12 +6,12 @@ echo $varId2
 echo $varId1
 #echo"`git diff $varId2 $varId1 test`"
 
-varOutput=`git diff $varId2 $varId1 test |head -1`
+varOutput=`git diff $varId2 $varId1 test |head -2|awk -F " " '{print $4}'`
 echo $varOutput
 
 if [ -n $varOutput ]
 then
-	echo "hiiiiiiiii"
+	echo "non zero string"
 else
-	echo "biiiiiiiiiiii"
+	echo "lenth zero string"
 fi
--- testscr.sh
+++ testscr.sh
@@ -6,4 +6,3 @@ echo $varImage_name
 echo $varIp_address
 #scp -pr test ec2-user@54.161.192.128:~/
 #ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
-#ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
