96061d76574e7aa26e3cf5b07b9ad66676808105
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 18:48:47 +0530

    delete content from file testscr.sh

--- diff.sh
+++ diff.sh
@@ -6,7 +6,8 @@ echo $varId2
 echo $varId1
 #echo"`git diff $varId2 $varId1 test`"
 
-varOutput=`git diff $varId2 $varId1 test`
+varOutput=`git diff $varId2 $varId1 test |head -1`
+echo $varOutput
 
 if [ -n $varOutput ]
 then
