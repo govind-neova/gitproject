cfa4503723f151f1e4b9bc169923e8aeac595367
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 17:46:30 +0530

    Deleting content from file test

---
+++ diff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+varId2=`git log |grep commit |head -2|tail -1|awk -F " " '{print $2}'`
+varId1=`git log |grep commit |head -1|awk -F " " '{print $2}'`
+echo $varId2
+echo $varId1
+echo "`git diff $varId2 $varId1 test`"
