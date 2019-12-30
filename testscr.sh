809196502204cdaad1bbdad80780987087d53fcc
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 13:03:22 +0530

    comment testscr.sh

--- testscr.sh
+++ testscr.sh
@@ -4,5 +4,5 @@ varImage_name=`cat test |grep -i tag|awk -F : '{print $2}'`
 varIp_address=`cat test |grep -i ip-address|awk -F : '{print $2}'`
 echo $varImage_name
 echo $varIp_address
-scp -pr test ec2-user@54.161.192.128:~/
-ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
+#scp -pr test ec2-user@54.161.192.128:~/
+#ssh ec2-user@$varIp_address "docker run `cat test |grep -i tag|awk -F : '{print $2}'` &>/dev/null &"
