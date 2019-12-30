674ef177c45b396578d68e020f8c6805bcbce49c
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 18:04:12 +0530

    Deleting content from file testscr.sh

--- diff.sh
+++ diff.sh
@@ -4,9 +4,11 @@ varId2=`git log |grep commit |head -2|tail -1|awk -F " " '{print $2}'`
 varId1=`git log |grep commit |head -1|awk -F " " '{print $2}'`
 echo $varId2
 echo $varId1
-varOutput=echo"`git diff $varId2 $varId1 test`"
+echo"`git diff $varId2 $varId1 test`"
 
-if [ echo"`git diff $varId2 $varId1 test`" ]
+if [ -z echo"`git diff $varId2 $varId1 test`" ]
 then
 	echo "hiiiiiiiii"
+else
+	echo "biiiiiiiiiiii"
 fi
