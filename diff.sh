aca412188d4526c5396157391f513b945bab9816
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-30 18:46:28 +0530

    delete content from file testscr.sh

--- diff.sh
+++ diff.sh
@@ -4,7 +4,7 @@ varId2=`git log |grep commit |head -2|tail -1|awk -F " " '{print $2}'`
 varId1=`git log |grep commit |head -1|awk -F " " '{print $2}'`
 echo $varId2
 echo $varId1
-echo"`git diff $varId2 $varId1 test`"
+#echo"`git diff $varId2 $varId1 test`"
 
 varOutput=`git diff $varId2 $varId1 test`
 
