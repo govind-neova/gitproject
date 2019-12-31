e3c00dfa6a375f719a4be0446431a1dd740b5ee6
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-31 14:30:38 +0530

    Added freestyle job

--- freestyle.sh
+++ freestyle.sh
@@ -7,7 +7,7 @@ echo $varId1
 #echo"`git diff $varId2 $varId1 test`"
 
 #varOutput=`git diff $varId2 $varId1 test |head -2|awk -F " " '{print $4}'`
-varOutput="$(git diff $varId2 $varId1 test)"
+varOutput="$(git diff b24a7ab224e665b 49ffc3f62c209 test |grep -o test|uniq)"
 echo $varOutput
 
 if [ $varOutput==" " ]
